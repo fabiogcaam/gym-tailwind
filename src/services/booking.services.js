59adecdaed18b922705135940ee6bb3759723808
Fabio García <fabiogcaam@gmail.com>
2024-10-14 20:41:31 +0200

    booking services

---
+++ src/services/booking.services.js
@@ -0,0 +1,32 @@
+import axios from 'axios'
+
+class BookingServices {
+
+    constructor() {
+        this.api = axios.create({
+            baseURL: `${import.meta.env.VITE_APP_API_URL}/bookings`
+        })
+
+        this.api.interceptors.request.use((config) => {
+            const storedToken = localStorage.getItem('authToken')
+
+            if (storedToken) {
+                config.headers = { Authorization: `Bearer ${storedToken}` }
+            }
+
+            return config
+        })
+    }
+
+    createBooking(classId) {
+        this.api.post('/create', classId)
+    }
+
+    finishedBooking(bookingId) {
+        this.api.post('/finished', bookingId)
+    }
+
+}
+
+const bookingServices = new BookingServices()
+export default bookingServices
---
+++ src/services/user.services.js
@@ -0,0 +1,31 @@
+import axios from 'axios'
+
+class UserService {
+
+    constructor() {
+        this.api = axios.create({
+            baseURL: `${import.meta.env.VITE_APP_API_URL}/users`
+        })
+
+        this.api.interceptors.request.use((config) => {
+            const storedToken = localStorage.getItem('authToken')
+
+            if (storedToken) {
+                config.headers = { Authorization: `Bearer ${storedToken}` }
+            }
+
+            return config
+        })
+    }
+
+    bookingList() {
+        return this.api.get('/bookings')
+    }
+
+    deleteBooking(bookingId) {
+        return this.api.post('/delete', bookingId)
+    }
+}
+
+const userService = new UserService()
+export default userService
