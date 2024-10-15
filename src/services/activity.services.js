dbecb1dc1047c8a9550df41a971ab1c4be37f6fc
Fabio García <fabiogcaam@gmail.com>
2024-10-15 16:47:31 +0200

    activity services

--- src/services/activity.services.js
+++ src/services/activity.services.js
@@ -6,10 +6,28 @@ class ActivityService {
         this.api = axios.create({
             baseURL: `${import.meta.env.VITE_APP_API_URL}/trips`
         })
+
+        this.api.interceptors.request.use((config) => {
+            const storedToken = localStorage.getItem('authToken')
+
+            if (storedToken) {
+                config.headers = { Authorization: `Bearer ${storedToken}` }
+            }
+
+            return config
+        })
     }
 
     getActivityList() {
         return this.api.get('/')
+    }
+
+    addActivity(activityInfo) {
+        return this.api.post('/add', activityInfo)
+    }
+
+    deleteActivity(idActivity) {
+        return this.api.post('delete', idActivity)
     }
 
 }
