e3f4abff1944df86c395e484d945a00ed03aac7b
Fabio García <fabiogcaam@gmail.com>
2024-10-23 17:37:14 +0200

    class service, get classes from date

--- src/components/Calendar/Calendar.jsx
+++ src/components/Calendar/Calendar.jsx
@@ -1,7 +1,28 @@
 import ReactCalendar from 'react-calendar'
+import { useState } from 'react'
+import classService from '../../services/class.services'
 
 
 const Calendar = () => {
+
+    const [date, setDate] = useState(new Date())
+    const [classes, setClasses] = useState([])
+
+    function handleDateChange(newDate) {
+        setDate(newDate)
+        getClassesAtDay(newDate)
+    }
+
+    function getClassesAtDay(selectedDate) {
+
+        const dayOfWeek = selectedDate.toLocaleString('en-US', { weekday: 'long' })
+
+        classService
+            .getClassesByDay(dayOfWeek)
+            .then(({ data }) => setClasses(data))
+            .catch(err => console.log(err))
+
+    }
 
     return (
         <div className='flex justify-center my-20 flex-col items-center'>
@@ -12,7 +33,7 @@ const Calendar = () => {
                 minDate={new Date()}
                 className="react-view"
                 view='month'
-                onClickDay={(value) => console.log(value)} />
+                onClickDay={(date) => handleDateChange(date)} />
 
         </div>
     )
---
+++ src/services/class.services.js
@@ -0,0 +1,37 @@
+import axios from 'axios'
+
+class ClassService {
+
+    constructor() {
+        this.api = axios.create({
+            baseURL: `${import.meta.env.VITE_APP_API_URL}/classes`
+        })
+
+        this.api.interceptors.request.use((config) => {
+            const storedToken = localStorage.getItem('authToken')
+
+            if (storedToken) {
+                config.headers = { Authorization: `Bearer ${storedToken}` }
+            }
+
+            return config
+        })
+    }
+
+    getClassesByDay({ day }) {
+        return this.api.get('/day', day)
+    }
+
+    addClass({ activityId, trainerId, schedule, participants, numParticipants }) {
+        return this.api.post('/add', { activityId, trainerId, schedule, participants, numParticipants })
+    }
+
+    deleteClass(id) {
+        return this.api.post(`/${id}/delete`)
+    }
+
+
+}
+
+const classService = new ClassService()
+export default classService
