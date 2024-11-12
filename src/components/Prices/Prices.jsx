894ad22e4f64a80951f4b345dff09e6a7fe7ca90
Fabio García <fabiogcaam@gmail.com>
2024-11-12 17:55:03 +0100

    responsive info and prices

--- src/components/Prices/Prices.jsx
+++ src/components/Prices/Prices.jsx
@@ -5,14 +5,14 @@ const Prices = () => {
 
     return (
 
-        <div className="Prices grid grid-flow-col h-256 gap-6 items-center relative">
+        <div className="flex flex-col h-auto justify-center items-center md:h-256 md:flex-row md:justify-around mt-40">
             {
                 PRICES.map((elm, index) => {
                     return (
                         <div key={index}
-                            className={`${COLORS[index % COLORS.length]} text-white text-center py-10 mx-16 rounded-xl h-128 w-80 flex flex-col items-center justify-center relative hover:z-10 transition-all duration-300 ease-in-out transform hover:scale-110`}>
-                            <h1 className="text-2xl mb-3">{elm.name}</h1>
-                            <h2 className="text-4xl my-5">{elm.price}€</h2>
+                            className={`${COLORS[index % COLORS.length]} text-white text-center my-5 py-10 rounded-xl h-128 w-80 flex flex-col items-center justify-center sm:my-5 relative hover:z-10 transition-all duration-300 ease-in-out transform hover:scale-110`}>
+                            <h1 className="text-3xl font-semibold mb-3">{elm.name}</h1>
+                            <h2 className="text-4xl my-5">{elm.price} €</h2>
                             <ul>
                                 {elm.plans.map((elm, index) => {
                                     return (
@@ -21,7 +21,7 @@ const Prices = () => {
                                 })}
                             </ul>
                             <Link to="/login">
-                                <div className="bg-white text-black mt-5 py-2 text-xl px-10 rounded-2xl text-center click:bg-grey-400 click:text-white">Añadir</div>
+                                <div className="bg-white text-black mt-5 py-2 text-xl px-10 rounded-2xl text-center hover:bg-violet hover:text-white">Añadir</div>
                             </Link>
                         </div>
                     )
