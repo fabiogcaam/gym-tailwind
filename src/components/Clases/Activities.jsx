498f0140f6e2191448cf8dc2c678bc2ed187f6f7
Fabio García <fabiogcaam@gmail.com>
2024-12-02 13:52:38 +0100

    getting activities from database

--- src/components/Clases/Activities.jsx
+++ src/components/Clases/Activities.jsx
@@ -29,14 +29,14 @@ const Activities = () => {
             <h1 className="underline underline-offset-8 mb-10 text-center text-2xl hover:decoration-violet">Actividades</h1>
             <div className="flex flex-col justify-center mx-36 sm:mx-0 md:grid md:grid-cols-3 md:gap-6">
                 {
-                    ACTIVITIES.map((elm, index) => {
+                    activities.map((elm, index) => {
                         console.log(index)
                         return (
                             <div key={index} className={`mx-5 my-10 md:mx-5 rounded-md perspective ${index === 6 && "col-start-2 flex justify-center items-center"}`}>
                                 <div className="relative w-4/6 mx-auto md:w-full h-64 transition-transform duration-500 transform-style-3d hover:rotate-y-180">
                                     <div className="absolute backface-hidden w-full h-full">
                                         <figure className="">
-                                            <img className="object-cover w-full h-72 transition-opacity duration-500 hover:opacity-0" src={elm.imageURL} alt={elm.name} />
+                                            <img className="object-cover w-full h-72 transition-opacity duration-500 hover:opacity-0" src={`${elm.imageUrl.replace(".webp", ".jpg")}`} alt={elm.name} />
                                         </figure>
                                     </div>
                                     <div className="absolute inset-0 flex flex-col h-72 justify-center items-center bg-violet bg-opacity-80 text-white opacity-0 transition-opacity duration-500 hover:opacity-100">
